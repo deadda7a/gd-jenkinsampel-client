b9bfd0a37ca8b9233f9240dd6eb6eecc98fa8259
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2017-06-15 19:00:34 +0200

    stuff

--- arduino/src/Main.cpp
+++ arduino/src/Main.cpp
@@ -1,74 +1,78 @@
 #include <ArduinoJson.h>
 #include <Ethernet.h>
 #include <SPI.h>
-#include <string.h>
+#include <TM1650.h>
+#include <Wire.h>
 
+TM1650 Disp4Seg;
+// #define SDA_pin 20
+// #define SCL_pin 21
 StaticJsonBuffer<200> jsonBuffer;
-String state = "off";
-float price = 0;
-
-String recvData() {
-  String endMarker = "\n";
-  String rc = "";
-  String receivedChars = "";
-
-  while (Serial.available() > 0) {
-    rc = Serial.readString();
-
-    if (rc != endMarker) {
-      receivedChars += rc;
-    } else {
-      return receivedChars;
-    }
-  }
-}
+char endMarker = '\n';
+int status = 0;
+int price = 0;
+bool building = false;
+bool jourfix = false;
 
 void setup() {
   Serial.begin(9600);
-  Serial.print("Arduino ready");
+  Wire.begin();
+  Wire.setClock(400000);
+  Disp4Seg.Init();
+  delay(1000);
+  Serial.println("Arduino ready");
+}
+
+String getJsonRaw() {
+  String receivedChars = "";
+  receivedChars = Serial.readStringUntil(endMarker);
+  return receivedChars;
+}
+
+void displayPrice(int price) {
+  Disp4Seg.SetBrightness(1);
+  Disp4Seg.SetDot(1, true);
+  Disp4Seg.WriteNum(price);
+  delay(5);
+  Disp4Seg.DisplayOFF();
 }
 
 void loop() {
-  String endMarker = "\n";
-
   if (Serial.available() > 0) {
-    String rc = "";
-    // char receivedChars[numChars];
-    String receivedChars = "";
+    char op = char(Serial.readStringUntil(endMarker)[0]);
 
-    rc = Serial.readString();
+    switch (op) {
+    case 'r': {
+      Serial.print("send\n");
 
-    if (rc != endMarker) {
-      receivedChars += rc;
-    } else {
-      Serial.print(receivedChars);
+    } break;
+
+    case 'w': {
+      Serial.print("recv\n");
+      String jsonString = getJsonRaw();
+
+      int count = jsonString.length();
+      char testArray[count];
+
+      jsonString.toCharArray(testArray, count);
+
+      JsonObject &root = jsonBuffer.parseObject(jsonString);
+
+      if (!root.success()) {
+        Serial.println("parseObject failed");
+      } else {
+        price = root["price"];
+        building = root["building"];
+        jourfix = root["jourfix"];
+        status = root["status"];
+
+        displayPrice(price);
+      }
+    } break;
+
+    default:
+      Serial.print("What the fuck?\n");
+      break;
     }
-
-    // char op = char(Serial.read());
-    //
-    // switch (op) {
-    // case 'r': {
-    //   Serial.print("send\n");
-    //
-    // } break;
-    // case 'w': {
-    //   Serial.print("recv\n");
-    //   String json = recvData();
-    //   Serial.print("recv1\n");
-    //   JsonObject &root = jsonBuffer.parseObject(json);
-    //   Serial.print("recv2\n");
-    //   if (!root.success()) {
-    //     Serial.println("parseObject() failed");
-    //     return;
-    //   }
-    //   Serial.print("recv3\n");
-    //   state = root["state"].as<String>();
-    //   price = root["price"];
-    // } break;
-    //
-    // default:
-    //   Serial.print("What the fuck?\n");
-    //   break;
-    // }
   }
 }
---
+++ rpi/jenkins.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+# http://jenkins.dev/api/json
+# http://jenkins.dev/job/vorsorgepreis/api/json
+
+jenkinsApiBase = "http://localhost/jenkins/"
