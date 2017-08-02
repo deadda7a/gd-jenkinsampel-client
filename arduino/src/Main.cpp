f0db13af77c7ab3a2257c9f54c1df7a0f2ce3e4b
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2017-08-02 18:51:23 +0200

    First working draft

--- arduino/src/Main.cpp
+++ arduino/src/Main.cpp
@@ -1,5 +1,4 @@
 #include <ArduinoJson.h>
-#include <Ethernet.h>
 #include <SPI.h>
 #include <TM1650.h>
 #include <Wire.h>
@@ -7,12 +6,43 @@
 TM1650 Disp4Seg;
 // #define SDA_pin 20
 // #define SCL_pin 21
-StaticJsonBuffer<200> jsonBuffer;
 char endMarker = '\n';
 int status = 0;
-int price = 0;
 bool building = false;
 bool jourfix = false;
+int price = 0;
+unsigned long time = 0;
+String output = "";
+
+#define RED 2
+#define YELLOW 3
+#define GREEN 4
+#define RED_BUILD 5
+#define YELLOW_BUILD 6
+#define GREEN_BUILD 7
+
+#define SW1 8
+#define SW2 9
+#define SW3 10
+#define SW4 11
+#define SW5 12
+#define SW6 13
+
+void displayPrice(int price) {
+  Disp4Seg.DisplayON();
+  Disp4Seg.SetBrightness(1);
+  Disp4Seg.SetDot(1, true);
+  Disp4Seg.WriteNum(price);
+}
+
+void resetRelais() {
+  digitalWrite(RED, HIGH);
+  digitalWrite(YELLOW, HIGH);
+  digitalWrite(GREEN, HIGH);
+  digitalWrite(RED_BUILD, HIGH);
+  digitalWrite(YELLOW_BUILD, HIGH);
+  digitalWrite(GREEN_BUILD, HIGH);
+}
 
 void setup() {
   Serial.begin(9600);
@@ -20,59 +50,91 @@ void setup() {
   Wire.setClock(400000);
   Disp4Seg.Init();
   delay(1000);
+
+  pinMode(RED, OUTPUT);
+  pinMode(YELLOW, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(RED_BUILD, OUTPUT);
+  pinMode(YELLOW_BUILD, OUTPUT);
+  pinMode(GREEN_BUILD, OUTPUT);
+
+  pinMode(SW1, INPUT);
+  pinMode(SW2, INPUT);
+  pinMode(SW3, INPUT);
+  pinMode(SW4, INPUT);
+  pinMode(SW5, INPUT);
+  pinMode(SW6, INPUT);
+
+  resetRelais();
+
   Serial.println("Arduino ready");
-}
-
-String getJsonRaw() {
-  String receivedChars = "";
-  receivedChars = Serial.readStringUntil(endMarker);
-  return receivedChars;
-}
-
-void displayPrice(int price) {
-  Disp4Seg.SetBrightness(1);
-  Disp4Seg.SetDot(1, true);
-  Disp4Seg.WriteNum(price);
-  delay(5);
-  Disp4Seg.DisplayOFF();
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    char op = char(Serial.readStringUntil(endMarker)[0]);
+    String input = Serial.readStringUntil(endMarker);
+    input.trim();
 
-    switch (op) {
-    case 'r': {
-      Serial.print("send\n");
+    if (input.length() == 6) {
+      time = millis();
 
-    } break;
+      status = input.substring(0, 1).toInt();
+      price = input.substring(3).toInt();
 
-    case 'w': {
-      Serial.print("recv\n");
-      String jsonString = getJsonRaw();
+      building = input.substring(1, 2).toInt() == 1;
+      jourfix = input.substring(3, 4).toInt() == 1;
 
-      int count = jsonString.length();
-      char testArray[count];
+      resetRelais();
 
-      jsonString.toCharArray(testArray, count);
+      if (building) {
+        switch (status) {
+        case 1:
+          digitalWrite(RED_BUILD, LOW);
+          break;
 
-      JsonObject &root = jsonBuffer.parseObject(jsonString);
+        case 2:
+          digitalWrite(YELLOW_BUILD, LOW);
+          break;
 
-      if (!root.success()) {
-        Serial.println("parseObject failed");
+        case 3:
+          digitalWrite(GREEN_BUILD, LOW);
+          break;
+        }
       } else {
-        price = root["price"];
-        building = root["building"];
-        jourfix = root["jourfix"];
-        status = root["status"];
+        switch (status) {
+        case 1:
+          digitalWrite(RED, LOW);
+          break;
 
+        case 2:
+          digitalWrite(YELLOW, LOW);
+          break;
+
+        case 3:
+          digitalWrite(GREEN, LOW);
+          break;
+        }
+      }
+
+      if (price != 0) {
         displayPrice(price);
       }
-    } break;
 
-    default:
-      Serial.print("What the fuck?\n");
-      break;
+      output = digitalRead(SW6);
+      output += digitalRead(SW5);
+      output += digitalRead(SW4);
+      output += digitalRead(SW3);
+      output += digitalRead(SW2);
+      output += digitalRead(SW1);
+
+      Serial.println(output);
+
+    } else {
+      Serial.println("wtf");
     }
+  }
+
+  if (millis() - time >= 5000) {
+    Disp4Seg.DisplayOFF();
   }
 }
--- docs/notes.md
+++ docs/notes.md
@@ -1,11 +1,14 @@
 # Wires
-## Ethernet
-* 3v3 – J1.8
-* GND – J1.1
-* D2 – J2.5 (INT)
-* D7 – J2.2 (RDY)
-* D10 – J1.6 (SCS)
-* spi.1 – J1.4 (MISO)
-* spi.3 – J1.5 (SCK aka SCLK)
-* spi.5 – J2.3 (RST)
-* spi.4 – J1.3 (MOSI)
+## Relais
+Arduino   Board
+2         1
+3         2
+4         3
+5         4
+6         5
+7         6
+
+## SPI 4 Segment
+Arduino   Board
+20 SDA    SDA
+21 SCL    SCL
